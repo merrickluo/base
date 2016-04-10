65f21127b6dc54b7bfb832cd44b77087204fd261
ahxxm <i@ahxxm.com>
2016-04-10 13:05:59 +0800

    slim command docs

--- ci-slim.sh
+++ ci-slim.sh
@@ -5,4 +5,5 @@ set -e
 wget https://github.com/cloudimmunity/docker-slim/releases/download/1.14/dist_linux.zip
 unzip dist_linux.zip
 
-yes | ./dist_linux/docker-slim build gentoo
+# show debug log and container log, with http-probe enabled
+yes | ./dist_linux/docker-slim --debug build -p --show-clogs gentoo
