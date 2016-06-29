4c2fc53a2195d73f2a6d53c94ae76c0532425182
ahxxm <i@ahxxm.com>
2016-06-29 11:44:01 +0800

    add asciidoc as build dep

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -10,7 +10,7 @@ LIBRESSL_VERSION=2.3.6
 mkdir -p /tmp
 
 # dependencies
-build_dep="curl gnupg build-base pcre-dev zlib-dev linux-headers openssl-dev"
+build_dep="curl gnupg build-base pcre-dev zlib-dev linux-headers openssl-dev asciidoc"
 run_dep="ca-certificates openssl pcre zlib"
 
 apk --no-cache add ${build_dep} ${run_dep}
