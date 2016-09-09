6983002b01250d9c75b205a51d1b29796e748440
ahxxm <i@ahxxm.com>
2016-09-09 13:30:26 +0800

    combine pip3 install

--- ipython/build.sh
+++ ipython/build.sh
@@ -2,7 +2,5 @@
 apk add --update py-pip openssl python3 
 apk add --update build-base zeromq-dev python-dev python3-dev freetype-dev libpng-dev
 ln -s /usr/include/locale.h /usr/include/xlocale.h
-pip3 install --no-cache-dir "ipython[all]"
-pip3 install --no-cache-dir pandas matplotlib
-
+pip3 install --no-cache-dir "ipython[all]" pandas matplotlib
 apk del build-base
